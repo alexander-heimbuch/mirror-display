2355ef560419d9144139fdc3e374cc5ce9331c0b
Alexander Heimbuch <aheimbuch@demandware.com>
2016-10-19 10:02:27 +0200

    improve travel

--- src/client/components/travel/travel.jsx
+++ src/client/components/travel/travel.jsx
@@ -9,12 +9,20 @@ import icons from'./icons';
 const Travel = ({travel}) => (
   <div className="travel-component component">
     { travel.length ? <h2 className="travel-headline">{config.travel.start} - {config.travel.destination}</h2> : null }
-      {travel.map(departure =>
-        <div className="travel-departure" key={departure.id}>
-          <div className="travel-icon" dangerouslySetInnerHTML={icons.train} />
-          <div className="travel-start travel-details">{departure.start}</div>
-          <div className="travel-details">{departure.duration}</div>
-          <div className="travel-details">{departure.traffic}</div>
+      {travel.map(connection =>
+        <div className="travel-connection" key={connection.id}>
+					<div className="travel-summary">
+						<div className="travel-icon" dangerouslySetInnerHTML={icons.train} />
+          	<div className="travel-duration"><span className="overall-departure">{connection.departure}</span><span className="overall-arrival">{connection.arrival}</span></div>
+					</div>
+					{connection.route.map(route =>
+						<div className="travel-routes" key={route.product}>
+							<span className="product">{route.product}</span>
+							<span className="departure">{route.departure}</span>
+							⇾
+							<span className="arrival">{route.arrival}</span>
+						</div>
+					)}
         </div>
       )}
   </div>
--- src/server/components/travel/train.js
+++ src/server/components/travel/train.js
@@ -1,42 +1,95 @@
 import Bluebird from 'bluebird'
+import Horseman from 'node-horseman'
 import config from 'config'
 
-import website from '../../utils/website'
+function parseLinks() {
+	var results = [];
+	$ = window.$ || window.jQuery
 
-const requestUrl = (start = '', destination = '') => {
-  let now = new Date()
-  let startDate = [now.getDate(), now.getMonth() + 1, now.getFullYear()].join('.')
-  let startTime = [now.getHours(), now.getMinutes()].join(':')
+	$('.timelink a').each(function () {
+		results.push($(this).attr('href'))
+	})
 
-  return `${config.get('travel.train.query')}${start}&REQ0JourneyStopsSID=&Z=${destination}&REQ0JourneyStopsZID=&date=${startDate}&time=${startTime}&timesel=depart`
+	return results
 }
 
+function parseDetails() {
+	$ = window.$ || window.jQuery
+	var route = []
+
+	var result = {}
+	$('.rline').each(function () {
+		var $step = $(this)
+
+		if ($step.hasClass('mot_inter')) {
+			route.push(result)
+			result = {}
+			return
+		}
+
+		if ($step.hasClass('routeEnd') || $step.hasClass('routeChange__IV')) {
+			result.arrival = parseTime($step)
+			return
+		}
+
+		if ($step.hasClass('routeStart') || $step.hasClass('routeChange')) {
+			result.departure = parseTime($step)
+			return
+		}
+
+		if ($step.hasClass('mot')) {
+			result.product = parseProduct($step)
+			return
+		}
+	})
+
+	route.push(result);
+
+	return {
+		departure: parseTime($('.routeStart')),
+		arrival: parseTime($('.routeEnd')),
+		route: route
+	}
+
+	function parseTime($time) {
+		var trimmed;
+		$time.find('.bold').remove()
+		trimmed = $time.text().trim()
+		return trimmed.match(/([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/)[0]
+	}
+
+	function parseProduct($product) {
+		return $product.text().trim()
+	}
+}
+
+const connections = (start, destination) => new Bluebird((resolve, reject) => {
+	new Horseman()
+		.open(config.get('travel.query'))
+		.type('input[name="REQ0JourneyStopsS0G"]', start)
+		.type('input[name="REQ0JourneyStopsZ0G"]', destination)
+		.click('.hauptbtn')
+		.waitForNextPage()
+		.evaluate(parseLinks)
+		.then(resolve)
+		.close()
+		.catch(reject);
+})
+
+const details = (url) => new Bluebird((resolve, reject) => {
+	new Horseman()
+		.open(url)
+		.evaluate(parseDetails)
+		.then(resolve)
+		.close()
+		.catch(reject);
+})
+
 export default () =>
-  website(requestUrl(config.get('travel.start'), config.get('travel.destination')))
-    .then(($) => {
-      // Get link to more results page
-      const later = $('a.later').attr('href')
-      return Bluebird.resolve(`${config.get('travel.train.base')}${later}`)
-    })
-    .then(website)
-    .then(($) => {
-      const results = []
-
-      $('.result tbody.boxShadow').each((index, stop) => {
-        if (index > 5) {
-          return
-        }
-
-        results.push({
-          start: $(stop).find('.time').first().text(),
-          traffic: $(stop).find('.products').first().text().trim(),
-          duration: $(stop).find('.duration').first().text().trim()
-        })
-      })
-
-      return Bluebird.resolve(results)
-    })
-    .then((departures) => {
+	connections(config.get('travel.start'), config.get('travel.destination'))
+		.then((links) => Bluebird.resolve(links.slice(0, 2)))
+		.map(details)
+		.then((departures) => {
       let id = 1
 
       departures = departures.map((departure) => {
